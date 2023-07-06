d7866cbe1897b0ebbc47e7591e3a14bdac569518
Safuze <rainydaypinpaf@gmail.com>
2023-07-06 08:59:40 +0000

    Обновлены файлы

--- test/data.sh
+++ test/data.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-
-output=$(curl http://127.0.0.1:5000/)
-
-expected_output="06.07.2023"
-
-if [ "$output" == "$expected_output" ]; then
-    echo "Все верно"
+#! /bin/bash
+ANSWER=$(curl http://127.0.0.1:5000/)
+if [ "$ANSWER" != "06.07.2023" ]; then
+exit 1
 else
-    echo "Неправильно"
-    exit 1
+echo "Tests PASS!"
 fi
